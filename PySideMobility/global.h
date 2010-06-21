c925d249bfd53c84d079974120d538eafff57ed2
Renato Filho <renato.filho@openbossa.org>
2010-06-21 17:49:10 -0300

    Created SystemInfo module.

--- PySideMobility/global.h
+++ PySideMobility/global.h
@@ -1,16 +1,15 @@
 // Sensors
 // QtMobility global header
 
-#include <qaccelerometer.h>
-#include <qambientlightsensor.h>
-#include <qcompass.h>
-#include <qmagnetometer.h>
-#include <qorientationsensor.h>
-#include <qproximitysensor.h>
-#include <qrotationsensor.h>
-#include <qsensor.h>
-#include <qsensorbackend.h>
-#include <qsensormanager.h>
-#include <qsensorplugin.h>
-#include <qtapsensor.h>
+#undef QT_NO_STL
+#undef QT_NO_STL_WCHAR
+#define qdoc
+#define QT_NO_DEBUG
+
+//Qt INCLUDES
+#include <Qt/QtCore>
+
+#include <qtmobilityglobal.h>
+#include <QtSystemInfo/qsysteminfo.h>
+#include <QtSystemInfo/qsystemdisplayinfo.h>
 
