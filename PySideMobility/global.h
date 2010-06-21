9fc690d2b26db4e5506c302be857ac8a21582949
Renato Filho <renato.filho@openbossa.org>
2010-06-21 17:01:16 -0300

    Created PySide-Mobility skeleton.

---
+++ PySideMobility/global.h
@@ -0,0 +1,16 @@
+// Sensors
+// QtMobility global header
+
+#include <qaccelerometer.h>
+#include <qambientlightsensor.h>
+#include <qcompass.h>
+#include <qmagnetometer.h>
+#include <qorientationsensor.h>
+#include <qproximitysensor.h>
+#include <qrotationsensor.h>
+#include <qsensor.h>
+#include <qsensorbackend.h>
+#include <qsensormanager.h>
+#include <qsensorplugin.h>
+#include <qtapsensor.h>
+
