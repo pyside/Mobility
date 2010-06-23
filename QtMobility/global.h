e6d7beadcef18beffb9d4963d35e6813c96a6d3e
Renato Filho <renato.filho@openbossa.org>
2010-06-23 11:11:21 -0300

    Create Location bindings.
    Removed 'Q' from classes names.

--- QtMobility/global.h
+++ QtMobility/global.h
@@ -4,5 +4,16 @@
 
 #include "pyside_global.h"
 #include <qmobilityglobal.h>
+
+// SysInfo
 #include <QtSystemInfo/qsysteminfo.h>
+
+// Location
+#include <QtLocation/QGeoAreaMonitor>
+#include <QtLocation/QGeoCoordinate>
+#include <QtLocation/QGeoPositionInfo>
+#include <QtLocation/QGeoPositionInfoSource>
+#include <QtLocation/QGeoSatelliteInfo>
+#include <QtLocation/QGeoSatelliteInfoSource>
+#include <QtLocation/QNmeaPositionInfoSource>
 
