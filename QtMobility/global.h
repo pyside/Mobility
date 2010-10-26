f8bc96a9859a72afdb080a2df0bc9e77b193c887
Lauro Neto <lauro.neto@openbossa.org>
2010-10-26 14:40:44 -0300

    Make QtLocation support 1.0 again.
    Based on Luciano Wolf's patch.

--- QtMobility/global.h
+++ QtMobility/global.h
@@ -11,80 +11,83 @@
 #include <qsysteminfo.h>
 
 // Location
-#include <QGeoAddress>
 #include <QGeoAreaMonitor>
-#include <QGeoBoundingArea>
-#include <QGeoBoundingBox>
-#include <QGeoBoundingCircle>
 #include <QGeoCoordinate>
-#include <QGeoManeuver>
-#include <QGeoMapCircleObject>
-#include <QGeoMapData>
-#include <QGeoMapGroupObject>
-#include <QGeoMapObject>
-#include <QGeoMapObjectInfo>
-#include <QGeoMapOverlay>
-#include <QGeoMappingManager>
-#include <QGeoMappingManagerEngine>
-#include <QGeoMapPixmapObject>
-#include <QGeoMapPolygonObject>
-#include <QGeoMapPolylineObject>
-#include <QGeoMapRectangleObject>
-#include <QGeoMapRouteObject>
-#include <QGeoMapTextObject>
-#include <QGeoPlace>
 #include <QGeoPositionInfo>
 #include <QGeoPositionInfoSource>
-#include <QGeoRoute>
-#include <QGeoRouteReply>
-#include <QGeoRouteRequest>
-#include <QGeoRouteSegment>
-#include <QGeoRoutingManager>
-#include <QGeoRoutingManagerEngine>
 #include <QGeoSatelliteInfo>
 #include <QGeoSatelliteInfoSource>
-#include <QGeoSearchManager>
-#include <QGeoSearchManagerEngine>
-#include <QGeoSearchReply>
-#include <QGeoServiceProvider>
-#include <QGeoServiceProviderFactory>
-#include <QGeoTiledMapData>
-#include <QGeoTiledMappingManagerEngine>
-#include <QGeoTiledMapReply>
-#include <QGeoTiledMapRequest>
-#include <QGraphicsGeoMap>
-#include <QLandmark>
-#include <QLandmarkAbstractRequest>
-#include <QLandmarkAttributeFilter>
-#include <QLandmarkBoxFilter>
-#include <QLandmarkCategory>
-#include <QLandmarkCategoryFetchByIdRequest>
-#include <QLandmarkCategoryFetchRequest>
-#include <QLandmarkCategoryFilter>
-#include <QLandmarkCategoryId>
-#include <QLandmarkCategoryIdFetchRequest>
-#include <QLandmarkCategoryRemoveRequest>
-#include <QLandmarkCategorySaveRequest>
-#include <QLandmarkExportRequest>
-#include <QLandmarkFetchByIdRequest>
-#include <QLandmarkFetchRequest>
-#include <QLandmarkFilter>
-#include <QLandmarkId>
-#include <QLandmarkIdFetchRequest>
-#include <QLandmarkIdFilter>
-#include <QLandmarkImportRequest>
-#include <QLandmarkIntersectionFilter>
-#include <QLandmarkManager>
-#include <QLandmarkManagerEngine>
-#include <QLandmarkManagerEngineFactory>
-#include <QLandmarkNameFilter>
-#include <QLandmarkNameSort>
-#include <QLandmarkProximityFilter>
-#include <QLandmarkRemoveRequest>
-#include <QLandmarkSaveRequest>
-#include <QLandmarkSortOrder>
-#include <QLandmarkUnionFilter>
 #include <QNmeaPositionInfoSource>
+
+#if (QTM_VERSION >= QTM_VERSION_CHECK(1, 1, 0))
+    #include <QGeoAddress>
+    #include <QGeoBoundingArea>
+    #include <QGeoBoundingBox>
+    #include <QGeoBoundingCircle>
+    #include <QGeoManeuver>
+    #include <QGeoMapCircleObject>
+    #include <QGeoMapData>
+    #include <QGeoMapGroupObject>
+    #include <QGeoMapObject>
+    #include <QGeoMapObjectInfo>
+    #include <QGeoMapOverlay>
+    #include <QGeoMappingManager>
+    #include <QGeoMappingManagerEngine>
+    #include <QGeoMapPixmapObject>
+    #include <QGeoMapPolygonObject>
+    #include <QGeoMapPolylineObject>
+    #include <QGeoMapRectangleObject>
+    #include <QGeoMapRouteObject>
+    #include <QGeoMapTextObject>
+    #include <QGeoPlace>
+    #include <QGeoRoute>
+    #include <QGeoRouteReply>
+    #include <QGeoRouteRequest>
+    #include <QGeoRouteSegment>
+    #include <QGeoRoutingManager>
+    #include <QGeoRoutingManagerEngine>
+    #include <QGeoSearchManager>
+    #include <QGeoSearchManagerEngine>
+    #include <QGeoSearchReply>
+    #include <QGeoServiceProvider>
+    #include <QGeoServiceProviderFactory>
+    #include <QGeoTiledMapData>
+    #include <QGeoTiledMappingManagerEngine>
+    #include <QGeoTiledMapReply>
+    #include <QGeoTiledMapRequest>
+    #include <QGraphicsGeoMap>
+    #include <QLandmark>
+    #include <QLandmarkAbstractRequest>
+    #include <QLandmarkAttributeFilter>
+    #include <QLandmarkBoxFilter>
+    #include <QLandmarkCategory>
+    #include <QLandmarkCategoryFetchByIdRequest>
+    #include <QLandmarkCategoryFetchRequest>
+    #include <QLandmarkCategoryFilter>
+    #include <QLandmarkCategoryId>
+    #include <QLandmarkCategoryIdFetchRequest>
+    #include <QLandmarkCategoryRemoveRequest>
+    #include <QLandmarkCategorySaveRequest>
+    #include <QLandmarkExportRequest>
+    #include <QLandmarkFetchByIdRequest>
+    #include <QLandmarkFetchRequest>
+    #include <QLandmarkFilter>
+    #include <QLandmarkId>
+    #include <QLandmarkIdFetchRequest>
+    #include <QLandmarkIdFilter>
+    #include <QLandmarkImportRequest>
+    #include <QLandmarkIntersectionFilter>
+    #include <QLandmarkManager>
+    #include <QLandmarkManagerEngine>
+    #include <QLandmarkManagerEngineFactory>
+    #include <QLandmarkNameFilter>
+    #include <QLandmarkNameSort>
+    #include <QLandmarkProximityFilter>
+    #include <QLandmarkRemoveRequest>
+    #include <QLandmarkSaveRequest>
+    #include <QLandmarkSortOrder>
+    #include <QLandmarkUnionFilter>
+#endif
 
 // Bearer
 #if(QT_VERSION < QT_VERSION_CHECK(4, 7, 0))
