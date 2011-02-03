27319aebd4bb28f0a48cf431b9f877ac87428854
Lauro Neto <lauro.neto@openbossa.org>
2011-02-03 11:47:26 -0300

    Do not compare SysInfo.Os to os.uname
    SysInfo.Os behavior isn't consistent with os.uname
    
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>

--- tests/SystemInfo/sysinfo_test.py
+++ tests/SystemInfo/sysinfo_test.py
@@ -6,7 +6,6 @@ from QtMobility.SystemInfo import QSystemInfo
 class QSystemInfoTest(unittest.TestCase):
     def testSystemInfo(self):
         sysInfo = QSystemInfo()
-        self.assertEqual(sysInfo.version(QSystemInfo.Os), os.uname()[2])
         self.assertEqual(sysInfo.version(QSystemInfo.QtCore), QtCore.__version__)
 
 if __name__ == '__main__':
