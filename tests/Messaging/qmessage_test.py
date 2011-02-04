f3dcd2e437461922f1e2dcda02e9b8cfa17c4ef6
Marcelo Lira <marcelo.lira@openbossa.org>
2011-02-04 09:30:13 -0300

    QMessage test must use a QApplication to work properly.
    Reviewed by Lauro Moura <lauro.neto@openbossa.org>
    Reviewed by Luciano Wolf <luciano.wolf@openbossa.org>

--- tests/Messaging/qmessage_test.py
+++ tests/Messaging/qmessage_test.py
@@ -1,8 +1,10 @@
 import unittest
+from helper import UsesQApplication
 from QtMobility.Messaging import *
 from PySide.QtCore import *
 
-class QMessageTest(unittest.TestCase):
+
+class QMessageTest(UsesQApplication):
 
     def testHeaderFields(self):
         msg = QMessage()
@@ -24,7 +26,7 @@ class QMessageTest(unittest.TestCase):
         msg.setPriority(QMessage.HighPriority)
         self.assertEquals(msg.priority(), QMessage.HighPriority)
         self.assertTrue(msg.isModified())
-        
+
         msg.setPriority(QMessage.LowPriority)
         self.assertEquals(msg.priority(), QMessage.LowPriority)
 
@@ -32,11 +34,11 @@ class QMessageTest(unittest.TestCase):
         msg = QMessage()
         self.assertEquals(msg.type(), QMessage.NoType)
         self.assertEquals(msg.isModified(), False)
-        
+
         msg.setType(QMessage.Email)
         self.assertEquals(msg.type(), QMessage.Email)
         self.assertTrue(msg.isModified())
-        
+
         msg.setType(QMessage.Mms)
         self.assertEquals(msg.type(), QMessage.Mms)
 
@@ -44,27 +46,27 @@ class QMessageTest(unittest.TestCase):
         msg = QMessage()
         self.assertEquals(msg.subject(), "")
         self.assertEquals(msg.isModified(), False)
-        
+
         subject = "Short subject"
         msg.setSubject(subject)
         self.assertEquals(msg.subject(), subject)
         self.assertTrue(msg.isModified())
-        
+
         subject = "A longer subject, just to try to make this test more interesting"
         msg.setSubject(subject)
         self.assertEquals(msg.subject(), subject)
-       
+
     def testFrom(self):
         msg = QMessage()
         self.assertEquals(msg.from_(), QMessageAddress())
         self.assertEquals(msg.isModified(), False)
-    
+
         addr = QMessageAddress(QMessageAddress.Email, "alice@example.org")
         msg.setFrom(addr)
         self.assertEquals(msg.from_(), addr)
         self.assertTrue(msg.from_() != QMessageAddress())
         self.assertTrue(msg.isModified())
-    
+
         addr = QMessageAddress(QMessageAddress.InstantMessage, "bob@example.org")
         msg.setFrom(addr)
         self.assertEquals(msg.from_(), addr)
@@ -75,15 +77,15 @@ class QMessageTest(unittest.TestCase):
         msg = QMessage()
         self.assertEquals(msg.to(), [])
         self.assertEquals(msg.isModified(), False)
-    
+
         addresses = []
         addresses.append(QMessageAddress(QMessageAddress.Email, "alice@example.org"))
         addresses.append(QMessageAddress(QMessageAddress.Email, "bob@example.org"))
-    
+
         msg.setTo(addresses)
         self.assertEquals(msg.to(), addresses)
         self.assertTrue(msg.isModified())
-    
+
         addresses = []
         addresses.append(QMessageAddress(QMessageAddress.System, "charlie@example.org"))
         msg.setTo(addresses)
@@ -94,33 +96,33 @@ class QMessageTest(unittest.TestCase):
         msg = QMessage()
         self.assertEquals(msg.cc(), [])
         self.assertEquals(msg.isModified(), False)
-    
+
         addresses = []
         addresses.append(QMessageAddress(QMessageAddress.Email, "alice@example.org"))
         addresses.append(QMessageAddress(QMessageAddress.Email, "bob@example.org"))
-    
+
         msg.setCc(addresses)
         self.assertEquals(msg.cc(), addresses)
         self.assertTrue(msg.isModified())
-    
+
         addresses = []
         addresses.append(QMessageAddress(QMessageAddress.System, "charlie@example.org"))
         msg.setCc(addresses)
         self.assertEquals(msg.cc(), addresses)
-    
+
     def testBcc(self):
         msg = QMessage()
         self.assertEquals(msg.bcc(), [])
         self.assertEquals(msg.isModified(), False)
-    
+
         addresses = []
         addresses.append(QMessageAddress(QMessageAddress.Email, "alice@example.org"))
         addresses.append(QMessageAddress(QMessageAddress.Email, "bob@example.org"))
-    
+
         msg.setBcc(addresses)
         self.assertEquals(msg.bcc(), addresses)
         self.assertTrue(msg.isModified())
-    
+
         addresses = []
         addresses.append(QMessageAddress(QMessageAddress.System, "charlie@example.org"))
         msg.setBcc(addresses)
@@ -130,7 +132,7 @@ class QMessageTest(unittest.TestCase):
         msg = QMessage()
         self.assertEquals(msg.date(), QDateTime())
         self.assertEquals(msg.isModified(), False)
-        
+
         now = QDateTime(QDateTime.fromString(QDateTime.currentDateTime().toString(Qt.ISODate), Qt.ISODate))
         msg.setReceivedDate(now)
         self.assertEquals(msg.receivedDate(), now)
